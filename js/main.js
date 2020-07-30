2ec728188a6c500fef3e786141f88b4e750cac88
la-vida-loca <808beats.info@gmail.com>
2020-07-30 15:31:46 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("hallo from alex");
+new WOW().init();
+$("#black1, #orange1").click(() => {
+	console.log("click");
+	$("#black1").addClass("hidden");
+	$("#orange1").addClass("hidden");
+});
